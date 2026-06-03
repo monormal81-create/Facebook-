592c2c63fd0f73764026eab001024b89eb66769d
monormal81-create <monormal81@gmail.com>
2026-06-03 08:55:22 +0100

    Add files via upload

---
+++ socialx/database/dependencies.py
@@ -0,0 +1,15 @@
+from database.database import SessionLocal
+
+
+def get_db():
+    """
+    Database session dependency.
+    """
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
